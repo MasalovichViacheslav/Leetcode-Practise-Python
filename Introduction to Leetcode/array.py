39c7832abf866b1da02b62e298ceecc3f4c03341
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-04-03 22:57:55 +0300

    solve task '914. X of a Kind in a Deck of Cards'

--- Introduction to Leetcode/array.py
+++ Introduction to Leetcode/array.py
@@ -246,3 +246,65 @@ There are no two adjacent flowers in flowerbed.
 # print(Solution().canPlaceFlowers([1,1,1], 2), "must be False")
 
 
+# https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/description/?envType=problem-list-v2&envId=array
+'''
+914. X of a Kind in a Deck of Cards
+
+You are given an integer array deck where deck[i] represents the number written on the ith card.
+Partition the cards into one or more groups such that:
+
+1. Each group has exactly x cards where x > 1, and
+2. All the cards in one group have the same integer written on them.
+
+Return true if such partition is possible, or false otherwise.
+
+
+Example 1:
+Input: deck = [1,2,3,4,4,3,2,1]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
+
+Example 2:
+Input: deck = [1,1,1,2,2,2,3,3]
+Output: false
+Explanation: No possible partition. 
+
+Constraints:
+1 <= deck.length <= 104
+0 <= deck[i] < 104
+'''
+
+# Solution
+# from math import gcd
+#
+#
+# class Solution(object):
+#     def hasGroupsSizeX(self, deck):
+#         """
+#         :type deck: List[int]
+#         :rtype: bool
+#         """
+#         deck_dictionary = {}
+#         for card in deck:
+#             deck_dictionary[card] = deck_dictionary.get(card, 0) + 1
+#
+#         if 1 in deck_dictionary.values():
+#             return False
+#         else:
+#             gr_common_divisor = gcd(*deck_dictionary.values())
+#             if gr_common_divisor == 1:
+#                 return False
+#             else:
+#                 return True
+#
+# # Testcases
+# print(Solution().hasGroupsSizeX([1,2,3,4,4,3,2,1]), "<---", True)
+# print(Solution().hasGroupsSizeX([1,1,1,2,2,2,3,3]), "<---", False)
+# print(Solution().hasGroupsSizeX([1,1,2,2,2,2,3,3]), "<---", True)
+# print(Solution().hasGroupsSizeX([4,1,3,3,2,1,4,2]), "<---", True)
+# print(Solution().hasGroupsSizeX([1,1]), "<---", True)
+# print(Solution().hasGroupsSizeX([1,2]), "<---", False)
+# print(Solution().hasGroupsSizeX([1,2,3,4,5]), "<---", False)
+# print(Solution().hasGroupsSizeX([3]), "<---", False)
+
+
