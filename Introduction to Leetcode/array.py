372424f9b2c9cc87bbf236719ad5e38688c6bde3
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-04-04 10:17:55 +0300

    solve task '2760. Longest Even Odd Subarray With Threshold'

--- Introduction to Leetcode/array.py
+++ Introduction to Leetcode/array.py
@@ -308,3 +308,113 @@ Constraints:
 # print(Solution().hasGroupsSizeX([3]), "<---", False)
 
 
+# https://leetcode.com/problems/longest-even-odd-subarray-with-threshold/description/?envType=problem-list-v2&envId=array
+'''
+2760. Longest Even Odd Subarray With Threshold
+
+You are given a 0-indexed integer array nums and an integer threshold.
+
+Find the length of the longest subarray of nums starting at index l and ending at index r (0 <= l <= r < nums.length) 
+that satisfies the following conditions:
+
+1. nums[l] % 2 == 0
+2. For all indices i in the range [l, r - 1], nums[i] % 2 != nums[i + 1] % 2
+3. For all indices i in the range [l, r], nums[i] <= threshold
+Return an integer denoting the length of the longest such subarray.
+
+Note: A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+Input: nums = [3,2,5,4], threshold = 5
+Output: 3
+Explanation: In this example, we can select the subarray that starts at l = 1 and ends at r = 3 => [2,5,4]. 
+This subarray satisfies the conditions.
+Hence, the answer is the length of the subarray, 3. We can show that 3 is the maximum possible achievable length.
+
+Example 2:
+Input: nums = [1,2], threshold = 2
+Output: 1
+Explanation: In this example, we can select the subarray that starts at l = 1 and ends at r = 1 => [2]. 
+It satisfies all the conditions and we can show that 1 is the maximum possible achievable length.
+
+Example 3:
+Input: nums = [2,3,4,5], threshold = 4
+Output: 3
+Explanation: In this example, we can select the subarray that starts at l = 0 and ends at r = 2 => [2,3,4]. 
+It satisfies all the conditions.
+Hence, the answer is the length of the subarray, 3. We can show that 3 is the maximum possible achievable length.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100 
+1 <= nums[i] <= 100 
+1 <= threshold <= 100
+'''
+# class Solution:
+#     def longestAlternatingSubarray(self, nums, threshold) -> int:
+#         max_length = 0
+#         l, r = None, None
+#         for index in range(len(nums)):
+#
+#             # start of subarray
+#             if nums[index] % 2 == 0 and nums[index] <= threshold and l is None:
+#                 l = index
+#                 print(f'[if     ], index - {index}, l - {l}, r - {r}')
+#                 # case when array includes only one even integer that is in the end of array
+#                 if index == len(nums) - 1 and max_length == 0:
+#                     return 1
+#
+#             # case when subarray is ended because of end of array
+#             elif l is not None and index == len(nums) - 1:
+#                 if nums[index] <= threshold and nums[index] % 2 != nums[index - 1] % 2:
+#                     r = index
+#                     print(f'[elif1.1], index - {index}, l - {l}, r - {r}')
+#                     if r - l + 1 > max_length:
+#                         max_length = r - l + 1
+#                 else:
+#                     r = index - 1
+#                     print(f'[elif1.2], index - {index}, l - {l}, r - {r}')
+#                     if r - l + 1 > max_length:
+#                         max_length = r - l + 1
+#
+#             # case when subarray is ended threshold or end of even-odd order in subarray
+#             elif (l is not None and nums[index] > threshold) or (
+#                     l is not None and nums[index] % 2 == nums[index - 1] % 2):
+#                 r = index - 1
+#                 print(f'[elif2.0], index - {index}, l - {l}, r - {r}')
+#                 if r - l + 1 > max_length:
+#                     max_length = r - l + 1
+#                 if nums[index] % 2 == 0 and nums[index] <= threshold:
+#                     l, r = index, None
+#                 else:
+#                     l, r = None, None
+#
+#             else:
+#                 print(f'[else   ], index - {index}, l - {l}, r - {r}')
+#
+#         return max_length
+
+
+# Testcases
+# print(Solution().longestAlternatingSubarray([3,2,5,4], 5), "-->", 3)
+# print(Solution().longestAlternatingSubarray([1,2], 2), "-->", 1)
+# print(Solution().longestAlternatingSubarray([2,3,4,9], 5), "-->", 3)
+# print(Solution().longestAlternatingSubarray([2], 4), "-->", 1)
+# print(Solution().longestAlternatingSubarray([4], 2), "-->", 0)
+# print(Solution().longestAlternatingSubarray([3], 5), "-->", 0)
+# print(Solution().longestAlternatingSubarray([5], 3), "-->", 0)
+# print(Solution().longestAlternatingSubarray([3,2,5,3,4,5,4,5,7,2,1,7], 5), "-->", 4)
+# print(Solution().longestAlternatingSubarray([3,2,5,3,4,5,4,7,2,1,2,1,2,1], 5), "-->", 6)
+# print(Solution().longestAlternatingSubarray([3,2,5,3,4,5,4,7,2,1,2,1,2,7], 5), "-->", 5)
+# print(Solution().longestAlternatingSubarray([3,3,3,2], 4), "-->", 1)
+# print(Solution().longestAlternatingSubarray([3,3,3,2], 1), "-->", 0)
+# print(Solution().longestAlternatingSubarray([3,3,3], 4), "-->", 0)
+# print(Solution().longestAlternatingSubarray([2,2], 4), "-->", 1)
+# print(Solution().longestAlternatingSubarray([4,4,4], 8), "-->", 1)
+# print(Solution().longestAlternatingSubarray([4,10,3], 10), "-->", 2)
+# print(Solution().longestAlternatingSubarray([2,10,5], 7), "-->", 1)
+
+
