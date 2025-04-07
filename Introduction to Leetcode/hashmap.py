a3ae2e0d846c985bea5f3da074bdbc72b90c820c
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-04-07 15:41:23 +0300

    solve task '3487. Maximum Unique Subarray Sum After Deletion'

--- Introduction to Leetcode/hashmap.py
+++ Introduction to Leetcode/hashmap.py
@@ -207,3 +207,68 @@ Constraints:
 # print(Solution().checkIfExist([-20,8,-6,-14,0,-19,14,4]), '-->', True)
 
 
+# https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/description/?envType=problem-list-v2&envId=hash-table
+'''
+3487. Maximum Unique Subarray Sum After Deletion
+
+You are given an integer array nums.
+
+You are allowed to delete any number of elements from nums without making it empty. After performing the deletions, 
+select a subarray of nums such that:
+
+1. All elements in the subarray are unique.
+2. The sum of the elements in the subarray is maximized.
+3. Return the maximum sum of such a subarray.
+ 
+
+Example 1:
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation:
+Select the entire array without deleting any element to obtain the maximum sum.
+
+Example 2:
+Input: nums = [1,1,0,1,1]
+Output: 1
+Explanation:
+Delete the element nums[0] == 1, nums[1] == 1, nums[2] == 0, and nums[3] == 1. Select the entire array [1] to obtain 
+the maximum sum.
+
+Example 3:
+Input: nums = [1,2,-1,-2,1,0,-1]
+Output: 3
+Explanation:
+Delete the elements nums[2] == -1 and nums[3] == -2, and select the subarray [2, 1] from [1, 2, 1, 0, -1] to obtain 
+the maximum sum.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+'''
+
+#Solution
+# from typing import List
+#
+#
+# class Solution:
+#     def maxSum(self, nums: List[int]) -> int:
+#         non_negative = {x for x in nums if x >= 0}
+#         if non_negative:
+#             return sum(non_negative)
+#         else:
+#             negative = {x for x in nums if x < 0}
+#             return max(negative)
+#
+# # Testcases
+# print(Solution().maxSum([1,2,3,4,5]), '-->', 15)
+# print(Solution().maxSum([1,1,0,1,1]), '-->', 1)
+# print(Solution().maxSum([1,2,-1,-2,1,0,-1]), '-->', 3)
+# print(Solution().maxSum([-6,-1,-2]), '-->', -1)
+# print(Solution().maxSum([-6,0,-1,-2]), '-->', 0)
+# print(Solution().maxSum([-6]), '-->', -6)
+# print(Solution().maxSum([6]), '-->', 6)
+# print(Solution().maxSum([0]), '-->', 0)
+
+
