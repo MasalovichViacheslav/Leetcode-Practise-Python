5cace043d0c04a10f00e5de581a37c5d61bc8d68
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-04-08 22:30:02 +0300

    solve task '1805. Number of Different Integers in a String'

--- Introduction to Leetcode/hashmap.py
+++ Introduction to Leetcode/hashmap.py
@@ -399,3 +399,75 @@ Constraints:
 # print(Solution().missingInteger([1, 1, 1, 1]), '-->', 2)
 
 
+# https://leetcode.com/problems/number-of-different-integers-in-a-string/description/?envType=problem-list-v2&envId=hash-table
+'''
+1805. Number of Different Integers in a String
+
+You are given a string word that consists of digits and lowercase English letters.
+You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". 
+Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+Return the number of different integers after performing the replacement operations on word.
+
+Two integers are considered different if their decimal representations without any leading zeros are different.
+
+ 
+
+Example 1:
+Input: word = "a123bc34d8ef34"
+Output: 3
+Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
+
+Example 2:
+Input: word = "leet1234code234"
+Output: 2
+
+Example 3:
+Input: word = "a1b01c001"
+Output: 1
+Explanation: The three integers "1", "01", and "001" all represent the same integer because
+the leading zeros are ignored when comparing their decimal values.
+ 
+
+Constraints:
+1 <= word.length <= 1000
+word consists of digits and lowercase English letters.
+'''
+# class Solution:
+#     def numDifferentIntegers(self, word: str) -> int:
+#         numbs_set = set()
+#         number = ''
+#         for char in word:
+#             if not number and char.isdigit() or number and char.isdigit():
+#                 number += char
+#             elif number and char.isalpha():
+#                 numbs_set.update({int(number)})
+#                 number = ''
+#
+#         if number:
+#             numbs_set.update({int(number)})
+#
+#         return len(numbs_set)
+
+# class Solution:
+#     def numDifferentIntegers(self, word: str) -> int:
+#         for char in word:
+#             if char.isalpha():
+#                 word = word.replace(char, " ")
+#
+#         x = set()
+#         for num in word.split():
+#             x.add(int(num))
+#         print(word)
+#         print(word.split(' '))
+#         print(x)
+#
+#         return len(x)
+
+# testcases
+# print(Solution().numDifferentIntegers("a123bc34d8ef34"), '-->', 3)
+# print(Solution().numDifferentIntegers("a1b01c001"), '-->', 1)
+# print(Solution().numDifferentIntegers("6"), '-->', 1)
+# print(Solution().numDifferentIntegers("g"), '-->', 0)
+
+
