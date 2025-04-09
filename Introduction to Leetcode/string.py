f234814f47069131597d0a4bfdf04ddd20abcb51
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-04-09 22:38:43 +0300

    solve task '434. Number of Segments in a String'

--- Introduction to Leetcode/string.py
+++ Introduction to Leetcode/string.py
@@ -396,3 +396,37 @@ s consists of lowercase English letters.
 # print(Solution().repeatedSubstringPattern('ab'), '-->', False)
 
 
+# https://leetcode.com/problems/number-of-segments-in-a-string/description/?envType=problem-list-v2&envId=string
+'''
+434. Number of Segments in a String
+
+Given a string s, return the number of segments in the string.
+A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+Example 1:
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+
+Example 2:
+Input: s = "Hello"
+Output: 1
+ 
+
+Constraints:
+
+0 <= s.length <= 300
+s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+The only space character in s is ' '.
+'''
+# class Solution:
+#     def countSegments(self, s: str) -> int:
+#         return len(s.split())
+#
+# # testcases
+# print(Solution().countSegments("Hello, my name is John"), '-->', 5)
+# print(Solution().countSegments(''), '-->', 0)
+# print(Solution().countSegments("Hello"), '-->', 1)
+
