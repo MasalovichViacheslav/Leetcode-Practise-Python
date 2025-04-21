24a0d1a4e906f2a2207fafc79267fdab5e8bf43d
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-04-21 09:22:32 +0300

    solve task '350. Intersection of Two Arrays II'

--- DSA studing (in acc. with Claude plan)/DictionariesAndHashTables.py
+++ DSA studing (in acc. with Claude plan)/DictionariesAndHashTables.py
@@ -210,3 +210,57 @@ strs[i] consists of lowercase English letters.
 # print(solution.groupAnagrams([""]), 'expected: ', [[""]])
 
 
+# https://leetcode.com/problems/intersection-of-two-arrays-ii/
+'''
+350. Intersection of Two Arrays II
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear 
+as many times as it shows in both arrays and you may return the result in any order. 
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into t
+he memory at once?
+'''
+# from typing import List
+# from collections import Counter
+#
+# class Solution:
+#     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#
+#         if len(nums1) >= len(nums2):
+#             longer, shorter = nums1, nums2
+#         else:
+#             longer, shorter = nums2, nums1
+#
+#         longer_counter = Counter(longer)
+#         shorter_counter = Counter(shorter)
+#
+#         res = []
+#         for number, repeats in shorter_counter.items():
+#             if number in longer_counter:
+#                 res += [number] * min(repeats, longer_counter[number])
+#
+#         return res
+
+# testcases
+# solution = Solution()
+# print(solution.intersect([1,2,2,1], [2,2]), 'expected:', [2,2])
+# print(solution.intersect([4,9,5], [9,4,9,8,4]), 'expected:', [9,4])
+# print(solution.intersect([1,2], [1,1]), 'expected:', [1])
+
