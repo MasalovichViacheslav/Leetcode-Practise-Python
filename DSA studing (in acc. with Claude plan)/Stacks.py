1eedd6277f86b6cba9ccd34792b0a62f3c83886d
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-05-01 22:57:09 +0300

    solve task '155. Min Stack'

--- DSA studing (in acc. with Claude plan)/Stacks.py
+++ DSA studing (in acc. with Claude plan)/Stacks.py
@@ -228,3 +228,107 @@ Constraints:
 # print(solution.dailyTemperatures([90,60,30]), 'expected:', [0,0,0])
 # print(solution.dailyTemperatures([90]), 'expected:', [0])
 
+# https://leetcode.com/problems/min-stack/
+'''
+155. Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+-2**31 <= val <= 2**31 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 10 ** 4 calls will be made to push, pop, top, and getMin.
+'''
+# # Solution 1
+# from collections import deque
+#
+# class MinStack:
+#
+#     def __init__(self):
+#         self.tuplesStack = deque()
+#
+#     def push(self, val: int) -> None:
+#         if not self.tuplesStack:
+#             self.tuplesStack.append((val, val))
+#         else:
+#             if val < self.tuplesStack[-1][1]:
+#                 self.tuplesStack.append((val, val))
+#             else:
+#                 self.tuplesStack.append((val, self.tuplesStack[-1][1]))
+#
+#     def pop(self) -> None:
+#         self.tuplesStack.pop()
+#
+#     def top(self) -> int:
+#         return self.tuplesStack[-1][0]
+#
+#     def getMin(self) -> int:
+#         return self.tuplesStack[-1][1]
+
+# Solution 2
+# class MinStack:
+#
+#     def __init__(self):
+#         self.tuplesStack = []
+#
+#     def push(self, val: int) -> None:
+#         if not self.tuplesStack:
+#             self.tuplesStack.append((val, val))
+#         else:
+#             if val < self.tuplesStack[-1][1]:
+#                 self.tuplesStack.append((val, val))
+#             else:
+#                 self.tuplesStack.append((val, self.tuplesStack[-1][1]))
+#
+#     def pop(self) -> None:
+#         self.tuplesStack.pop()
+#
+#     def top(self) -> int:
+#         return self.tuplesStack[-1][0]
+#
+#     def getMin(self) -> int:
+#         return self.tuplesStack[-1][1]
+
+
+# minstack = MinStack()
+# minstack.push(-2)
+# minstack.push(0)
+# minstack.push(-3)
+# print(minstack.tuplesStack)
+# print(minstack.getMin())
+# minstack.pop()
+# print(minstack.top())
+# print(minstack.getMin())
+
+
