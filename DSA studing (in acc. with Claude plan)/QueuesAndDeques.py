7f91e78d7b21205f1fdf3c2465085b039aa234e2
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-05-01 10:36:40 +0300

    solve task '933. Number of Recent Calls'

--- DSA studing (in acc. with Claude plan)/QueuesAndDeques.py
+++ DSA studing (in acc. with Claude plan)/QueuesAndDeques.py
@@ -93,3 +93,116 @@ Depending on your language, the queue may not be supported natively. You may sim
 # print(mystack.empty())
 
 
+# https://leetcode.com/problems/number-of-recent-calls/
+'''
+933. Number of Recent Calls
+
+You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+Implement the RecentCounter class:
+
+RecentCounter() Initializes the counter with zero recent requests.
+int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of 
+requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number 
+of requests that have happened in the inclusive range [t - 3000, t].
+It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+
+ 
+Example 1:
+Input
+["RecentCounter", "ping", "ping", "ping", "ping"]
+[[], [1], [100], [3001], [3002]]
+Output
+[null, 1, 2, 3, 3]
+Explanation
+RecentCounter recentCounter = new RecentCounter();
+recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
+recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
+recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
+recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+ 
+
+Constraints:
+
+1 <= t <= 10 ** 9
+Each test case will call ping with strictly increasing values of t.
+At most 10 ** 4 calls will be made to ping.
+'''
+# Solution1
+# from collections import deque
+#
+# class RecentCounter:
+#
+#     def __init__(self):
+#         self.counter = deque()
+#
+#     def ping(self, t: int) -> int:
+#         self.counter.append(t)
+#         temp_counter = deque()
+#         latest_time = self.counter[-1]
+#         while self.counter:
+#             time = self.counter.pop()
+#             if latest_time - 3000 <= time:
+#                 temp_counter.appendleft(time)
+#             else:
+#                 break
+#         self.counter = temp_counter
+#         return len(self.counter)
+
+# Solution2
+# class RecentCounter:
+#
+#     def __init__(self):
+#         self.counter = []
+#
+#     def ping(self, t: int) -> int:
+#         temp_counter = [t]
+#         while self.counter:
+#             time = self.counter.pop()
+#             if t - 3000 <= time:
+#                 temp_counter.append(time)
+#             else:
+#                 break
+#         self.counter = temp_counter[::-1]
+#         return len(self.counter)
+
+# Solution3
+# class RecentCounter:
+#
+#     def __init__(self):
+#         self.counter = []
+#
+#     def ping(self, t: int) -> int:
+#         self.counter.append(t)
+#         for ind in range(-2, -len(self.counter) - 1, -1):
+#             if t - 3000 > self.counter[ind]:
+#                 self.counter = self.counter[ind + 1:]
+#                 break
+#         return len(self.counter)
+
+# Solution4
+# from collections import deque
+#
+# class RecentCounter:
+#
+#     def __init__(self):
+#         self.counter = deque()
+#
+#     def ping(self, t: int) -> int:
+#         self.counter.append(t)
+#         while self.counter[0] < t - 3000:
+#             self.counter.popleft()
+#         return len(self.counter)
+
+# testcases
+# obj = RecentCounter()
+# print(obj.ping(1))
+# print(obj.ping(100))
+# print(obj.ping(3001))
+# print(obj.ping(3002))
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+
