08e0c25cd14d2077700a4925e1181ccfa12a3d5a
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-05-02 10:11:51 +0300

    solve task '232. Implement Queue using Stacks'

--- DSA studing (in acc. with Claude plan)/QueuesAndDeques.py
+++ DSA studing (in acc. with Claude plan)/QueuesAndDeques.py
@@ -206,3 +206,89 @@ At most 10 ** 4 calls will be made to ping.
 # param_1 = obj.ping(t)
 
 
+# https://leetcode.com/problems/implement-queue-using-stacks/description/
+'''
+232. Implement Queue using Stacks
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the 
+functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty 
+operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque 
+(double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid.
+'''
+# # Solution1
+# class MyQueue:
+#
+#     def __init__(self):
+#         self.main_stack = []
+#         self.support_stack = []
+#
+#     def push(self, x: int) -> None:
+#         self.main_stack.append(x)
+#
+#     def pop(self) -> int:
+#         while self.main_stack:
+#             self.support_stack.append(self.main_stack.pop())
+#         elem = self.support_stack.pop()
+#         while self.support_stack:
+#             self.main_stack.append(self.support_stack.pop())
+#         return elem
+#
+#     def peek(self) -> int:
+#         while self.main_stack:
+#             self.support_stack.append(self.main_stack.pop())
+#         elem = self.support_stack.pop()
+#         self.main_stack.append(elem)
+#         while self.support_stack:
+#             self.main_stack.append(self.support_stack.pop())
+#         return elem
+#
+#     def empty(self) -> bool:
+#         if self.main_stack:
+#             return False
+#         else:
+#             return True
+
+
+# testcases
+# myQueue = MyQueue()
+# myQueue.push(1)
+# myQueue.push(2)
+# print(myQueue.peek())
+# print(myQueue.pop())
+# print(myQueue.empty())
+
+
