08134c5b6440ffd05449cbca554107feafd5aca7
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-05-03 22:17:37 +0300

    solve task '21. Merge Two Sorted Lists'

--- DSA studing (in acc. with Claude plan)/LinkedLIsts.py
+++ DSA studing (in acc. with Claude plan)/LinkedLIsts.py
@@ -49,3 +49,74 @@ Follow up: A linked list can be reversed either iteratively or recursively. Coul
 #         return new_head
 
 
+# https://leetcode.com/problems/merge-two-sorted-lists/description/
+'''
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+'''
+# Solution 1
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#
+#         if not list1:
+#             return list2
+#         elif not list2:
+#             return list1
+#
+#         merged = None
+#
+#         while list1 and list2:
+#
+#             if not merged:
+#                 if list1.val <= list2.val:
+#                     merged = list1
+#                     list1 = list1.next
+#                 else:
+#                     merged = list2
+#                     list2 = list2.next
+#                 new_header = merged
+#             else:
+#                 if list1.val <= list2.val:
+#                     merged.next = list1
+#                     list1 = list1.next
+#                 else:
+#                     merged.next = list2
+#                     list2 = list2.next
+#                 merged = merged.next
+#
+#         if list1:
+#             merged.next = list1
+#         else:
+#             merged.next = list2
+#
+#         return new_header
+
+
