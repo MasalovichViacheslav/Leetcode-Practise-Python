bdc3db7bb6e1b106cfa4f1dc7ec17752c82bf7a9
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-05-13 16:42:38 +0300

    solve task '145. Binary Tree Postorder Traversal'

--- DSA studing (in acc. with Claude plan)/TreesTraversal.py
+++ DSA studing (in acc. with Claude plan)/TreesTraversal.py
@@ -123,3 +123,80 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
 #         return order
 
 
+# https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+'''
+145. Binary Tree Postorder Traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+Follow up: Recursive solution is trivial, could you do it iteratively?
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#Solution 1
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#
+#         if not root:
+#             return []
+#
+#         stack = [[root, False]]
+#         order = []
+#
+#         while stack:
+#             node = stack.pop()
+#             if not node[1]:
+#                 stack.append([node[0], True])
+#                 if node[0].right:
+#                     stack.append([node[0].right, False])
+#                 if node[0].left:
+#                     stack.append([node[0].left, False])
+#             else:
+#                 order.append(node[0].val)
+#
+#         return order
+
+#Solution2
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#
+#         order = []
+#
+#         def postorder(node):
+#             if not node:
+#                 return
+#
+#             postorder(node.left)
+#             postorder(node.right)
+#             order.append(node.val)
+#
+#         postorder(root)
+#
+#         return order
+
+
