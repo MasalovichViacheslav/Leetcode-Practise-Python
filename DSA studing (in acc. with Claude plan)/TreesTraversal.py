94ef4604777a58d08195f357dffaa143b049018d
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-05-13 22:54:07 +0300

    solve task '111. Minimum Depth of Binary Tree'

--- DSA studing (in acc. with Claude plan)/TreesTraversal.py
+++ DSA studing (in acc. with Claude plan)/TreesTraversal.py
@@ -200,3 +200,56 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
 #         return order
 
 
+# https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+'''
+111. Minimum Depth of Binary Tree
+
+Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+Note: A leaf is a node with no children.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+
+Example 2:
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [0, 105].
+-1000 <= Node.val <= 1000
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#Solution1
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#
+#         if not root:
+#             return 0
+#
+#         stack = [[root]]
+#         levels = 0
+#
+#         while stack:
+#             levels += 1
+#             level_nodes = stack.pop()
+#             next_level_nodes = []
+#
+#             while level_nodes:
+#                 node = level_nodes.pop()
+#                 if node.left:
+#                     next_level_nodes.append(node.left)
+#                 if node.right:
+#                     next_level_nodes.append(node.right)
+#                 if not node.left and not node.right:
+#                     return levels
+#
+#             stack.append(next_level_nodes)
+
