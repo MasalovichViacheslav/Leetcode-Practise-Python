671321b41e4ff9303ff279323d66b5bf7657fabf
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-08-15 23:47:34 +0300

    solve task '74. Search a 2D Matrix'

--- DSA studing (in acc. with Claude plan)/BinarySearch.py
+++ DSA studing (in acc. with Claude plan)/BinarySearch.py
@@ -25,6 +25,8 @@ Constraints:
 All the integers in nums are unique.
 nums is sorted in ascending order.
 """
+from numpy.matrixlib.defmatrix import matrix
+
 # Solution 1
 # from typing import List
 # import bisect
@@ -197,4 +199,164 @@ nums contains distinct values sorted in ascending order.
 # print(solution.searchInsert([1,3,5,6], 2), "expected:", 1)
 # print(solution.searchInsert([1,3,5,6], 7), "expected:", 4)
 # print(solution.searchInsert([1,3,8,11,12,20,23,30], 22), "expected:", 6)
+
+
+
+# https://leetcode.com/problems/search-a-2d-matrix/description/
+"""
+74. Search a 2D Matrix
+
+You are given an m x n integer matrix matrix with the following two properties:
+1) Each row is sorted in non-decreasing order.
+2) The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-10**4 <= matrix[i][j], target <= 10**4
+"""
+# Solution 1
+# from typing import List
+# import bisect
+#
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#
+#         if target > matrix[-1][-1] or target < matrix[0][0]:
+#             return False
+#
+#         row_ind = bisect.bisect_left(matrix, target, key=lambda x: x[0])
+#
+#         if row_ind > len(matrix) - 1:
+#             row = matrix[row_ind - 1]
+#         elif matrix[row_ind][0] == target:
+#             return True
+#         else:
+#             row = matrix[row_ind - 1]
+#
+#         index = bisect.bisect_left(row, target)
+#         if index <= len(row) - 1 and row[index] == target:
+#             return True
+#         else:
+#             return False
+
+# Solution 2
+# from typing import List
+#
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         m_low, m_high = 0, len(matrix) - 1
+#
+#         while m_high >= m_low:
+#             middle = (m_high + m_low) // 2
+#             if matrix[middle][0] == target:
+#                 return True
+#             elif matrix[middle][0] > target:
+#                 m_high = middle - 1
+#             else:
+#                 m_low = middle + 1
+#
+#         if m_low == 0:
+#             row_ind = m_low
+#         else:
+#             row_ind = m_low - 1
+#
+#         row = matrix[row_ind]
+#
+#         r_low, r_high = 0, len(row) - 1
+#
+#         while r_high >= r_low:
+#             middle = (r_high + r_low) // 2
+#
+#             if row[middle] == target:
+#                 return True
+#             elif row[middle] > target:
+#                 r_high = middle - 1
+#             else:
+#                 r_low = middle + 1
+#
+#         return False
+
+
+# Solution 3
+# from typing import List
+#
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         def search_row_ind(matrix, target, low, high):
+#
+#             if low > high:
+#                 return low
+#
+#             middle = (low + high) // 2
+#
+#             if matrix[middle][0] == target:
+#                 return True
+#             elif matrix[middle][0] > target:
+#                 high = middle - 1
+#             else:
+#                 low = middle + 1
+#
+#             return search_row_ind(matrix, target, low, high)
+#
+#         result = search_row_ind(matrix, target, low=0, high=len(matrix) - 1)
+#
+#         if type(result) is bool:
+#             return True
+#         else:
+#             row_ind = result
+#
+#         if row_ind == 0:
+#             row = matrix[row_ind]
+#         else:
+#             row = matrix[row_ind - 1]
+#
+#         def search_target_in_row(row, target, r_low, r_high):
+#             if r_low > r_high:
+#                 return False
+#
+#             middle = (r_low + r_high) // 2
+#
+#             if row[middle] == target:
+#                 return True
+#             elif row[middle] > target:
+#                 r_high = middle - 1
+#             else:
+#                 r_low = middle + 1
+#
+#             return search_target_in_row(row, target, r_low, r_high)
+#
+#         return search_target_in_row(row, target, r_low=0, r_high=len(row) - 1)
+
+
+# solution = Solution()
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3), 'expected:', True)
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 34), 'expected:', True)
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13), 'expected:', False)
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 0), 'expected:', False)
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 61), 'expected:', False)
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 1), 'expected:', True)
+# print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,50]], 11), 'expected:', True)
+# print(solution.searchMatrix([[1,3,5,7]], 61), 'expected:', False)
+# print(solution.searchMatrix([[1,3,5,7]], 5), 'expected:', True)
+# print(solution.searchMatrix([[1],[11],[34]], 61), 'expected:', False)
+# print(solution.searchMatrix([[1],[11],[34]], 11), 'expected:', True)
+# print(solution.searchMatrix([[34]], 34), 'expected:', True)
+
+
 
